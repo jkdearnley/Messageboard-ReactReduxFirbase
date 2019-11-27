86fd9a94f5fe9c9be4b839103d58142a3d4881f2
jkdearnley <jkdearnley@gmail.com>
2019-11-28 12:36:14 +1300

    Initial setting up folders/files

--- messageboard/src/App.js
+++ messageboard/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import NavBar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+import DiscussionDetails from './components/discussions/DiscussionDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+import CreateDiscussion from './components/discussions/CreateDiscussion'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Message Board</h1>
+      </div>
+    </BrowserRouter>
+    );
+  }
 }
 
 export default App;
