7c871c91e97d39cf03dd8b335819efc34f2bafe3
jkdearnley <jkdearnley@gmail.com>
2019-11-28 13:01:42 +1300

    Signed In Links added

---
+++ messageboard/src/components/layout/SignedInLinks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const SignedInLinks = () => {
+    return (
+        <ul className="right">
+            <li><NavLink to='/'>Create Discussion</NavLink></li>
+            <li><NavLink to='/'>Log Out</NavLink></li>
+            <li><NavLink to='/' className="btn btn-floating green darken-2">JD</NavLink></li>
+        </ul>
+
+    )
+}
+
+export default SignedInLinks
