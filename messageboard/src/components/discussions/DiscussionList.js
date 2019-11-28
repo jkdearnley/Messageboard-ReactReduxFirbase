bf4ad57c653f94d29b5334bf20c8cb52a4be2bbf
jkdearnley <jkdearnley@gmail.com>
2019-11-28 13:35:03 +1300

    Initial dashboard items started

---
+++ messageboard/src/components/discussions/DiscussionList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const DiscussionList = () => {
+    return (
+        <div className="discussion-list section">
+            <div className="card z-depth-0 discussion-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Discussion Title</span>
+                    <p>Posted by JD</p>
+                    <p className="grey-text">29th August</p>
+                </div>
+            </div>    
+
+            <div className="card z-depth-0 discussion-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Discussion Title</span>
+                    <p>Posted by JD</p>
+                    <p className="grey-text">29th August</p>
+                </div>
+            </div> 
+
+            <div className="card z-depth-0 discussion-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Discussion Title</span>
+                    <p>Posted by JD</p>
+                    <p className="grey-text">29th August</p>
+                </div>
+            </div> 
+
+            <div className="card z-depth-0 discussion-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Discussion Title</span>
+                    <p>Posted by JD</p>
+                    <p className="grey-text">29th August</p>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+export default DiscussionList
