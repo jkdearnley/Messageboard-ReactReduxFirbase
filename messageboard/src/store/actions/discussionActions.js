d04d4a63b57c26e1180566ab84453265e0c51a62
jkdearnley <jkdearnley@gmail.com>
2019-12-01 17:09:39 +1300

    More firebase DB prep

--- messageboard/src/store/actions/discussionActions.js
+++ messageboard/src/store/actions/discussionActions.js
@@ -1,5 +1,5 @@
 export const createDiscussion = (discussion) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to database
         dispatch({ type: 'CREATE_DISCUSSION', discussion});
     }
